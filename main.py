5aa75e27f09c70edb2b506fe0516b9aa38ef175e
PedroH183 <pedro_freires18@outlook.com>
2025-04-18 22:57:13 -0300

    feat: automatic load dotenv

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+from dotenv import load_dotenv
 import uvicorn
 from fastapi import FastAPI
-
 
 app = FastAPI(
     title="Image Server Compressor Api", 
     description="API to receive images and publish into a Redis Streams",
 )
+load_dotenv()
+
+
 @app.get("/ping")
 async def ping():
     return {"status": "OK"}
