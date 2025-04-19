ca9daf7d8cf9de12c5eb1fd6a9c76664ade78e8f
PedroH183 <pedro_freires18@outlook.com>
2025-04-19 15:16:16 -0300

    refactor: clean up S3BucketsImages class by removing unused schema and improving formatting

--- s3_buckets_images_manager.py
+++ s3_buckets_images_manager.py
@@ -1,16 +1,10 @@
 import os
 import boto3
-from pydantic import BaseModel
-from typing import Optional, Union
+from schemas import S3GetSchema, S3UploadSchema
 
-class S3UploadSchema(BaseModel):
-    image_id: str
-    content: bytes
-    filename: Union[str, None]
-    content_type: Union[str, None]
 
 class S3BucketsImages:
-    """ Class responsible to connection with S3Buckets """
+    """Class responsible to connection with S3Buckets"""
 
     _instance = None
 
@@ -18,19 +12,25 @@ class S3BucketsImages:
     def get_client(cls):
         if cls._instance is None:
             s3_client = boto3.client(
-                's3',
-                region_name          = os.getenv('AWS_REGION'),
-                aws_access_key_id    = os.getenv('AWS_ACCESS_KEY_ID'),
-                aws_secret_access_key= os.getenv('AWS_SECRET_ACCESS_KEY'),
+                "s3",
+                region_name=os.getenv("AWS_REGION"),
+                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY"),
             )
         return s3_client
 
     @classmethod
     def put_object(cls, s3_client, params: S3UploadSchema):
         s3_client.put_object(
-            Body   =  params.content,
-            ContentType = params.content_type,
-            Bucket =  os.getenv('AWS_BUCKET_NAME'),
-            Key    =  f'images/{params.image_id}/{params.filename}',
+            Body=params.content,
+            ContentType=params.content_type,
+            Bucket=os.getenv("AWS_BUCKET_NAME"),
+            Key=f"images/{params.image_id}/{params.filename}",
         )
         return True
+
+    @classmethod
+    def get_object(cls, s3_client, params: S3GetSchema):
+        return s3_client.get_object(
+            Bucket=os.getenv("AWS_BUCKET_NAME"), Key=params.s3_key
+        )
