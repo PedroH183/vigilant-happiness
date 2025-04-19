8a5c9c20fa668ffcce422f6bcda725ec899d2580
PedroH183 <pedro_freires18@outlook.com>
2025-04-19 15:16:26 -0300

    feat: expand schemas with S3UploadSchema and S3GetSchema for enhanced image handling

--- schemas.py
+++ schemas.py
@@ -1,19 +1,48 @@
-from pydantic import BaseModel
 from enum import Enum
+from typing import List, Union
+from pydantic import BaseModel, field_validator
+
 
 class StatusEnum(str, Enum):
     SUCCESS = "success"
     ERROR = "error"
 
+
 class StreamsName(str, Enum):
     UPLOAD = "imagem_upload"
     COMPRESSAO = "compressao_concluida"
+
 
 class ImageUploadResponse(BaseModel):
     status: StatusEnum
     message: str
     image_id: str
 
+
 class ErrorResponse(BaseModel):
     status: StatusEnum
     message: str
+
+
+class S3UploadSchema(BaseModel):
+    image_id: str
+    content: bytes
+    filename: Union[str, None]
+    content_type: Union[str, None]
+
+    @field_validator("filename")
+    def filename_validator(filename):
+        return filename if filename else ""
+
+
+class S3GetSchema(BaseModel):
+    s3_key: str
+
+
+class ImageIndex(BaseModel):
+    image_id: str
+    filename: str
+
+class ImageIndexResponse(BaseModel):
+    status: str
+    indexes: List[ImageIndex]
